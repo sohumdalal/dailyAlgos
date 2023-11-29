193dcce4b411f613a4f22edbb69f0f893bb16a13
sohumdalal <dsohum@gmail.com>
2023-11-29 12:28:47 +0530

    small formatting

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -9,7 +9,6 @@ and "fizzbuzz" in place of numbers divisble by both 3 and 5.
 fizzbuzz(16);
 -> [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz',
 11, 'fizz', 13, 14, 'fizzbuzz', 16]
-
 */
 
 const fizzbuzz = num => {  
@@ -81,9 +80,9 @@ return final;
 
 /*
     Algo reflection:
-        this one was a good welcome back to algo life. still is very straightfoward for me to do, so I decided 
-        to throw in a few ternaries for giggles. I must remember to read the prompt, look at return outputs, and
-        continue to handle edge casing. All in all, classic and straight forward.
+        This one was a good welcome back to algo life after some vacation time. still is very straightfoward for me to do,
+         so I decided to throw in a few ternaries for giggles. I must remember to read the prompt, look at return outputs, 
+        and continue to handle edge casing. All in all, classic and straight forward.
 */
 
 module.exports = {fizzbuzz, fizzbuzzbazz};
