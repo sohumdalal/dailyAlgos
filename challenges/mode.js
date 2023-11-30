c1102e5efbcbb454a235724dbef8efa7ba6a9406
sohumdalal <dsohum@gmail.com>
2023-11-30 08:31:01 +0530

    cleared mode challenges

--- challenges/mode.js
+++ challenges/mode.js
@@ -12,38 +12,9 @@ mode([7, 5, 8, 8, 2, 5]) -> 8
 */
 
 const mode = array => {
-    //first store each number in a cache. the math.max the cache[num] 
-    cache = {};
-    array.forEach((element) => {
-        if (cache[element]) {
-            cache[element] += 1;
-        }
-        else {
-            cache[element] = 1
-        }
-    })
-    // console.log(cache);
-
-    const keys = Object.keys(cache);
-    let max = -Infinity;
-    let maxKey;
-
-    for (let i = 0; i < keys.length; i++) {
-        if (cache[keys[i]] > max) {
-            max = cache[keys[i]]
-            maxKey = keys[i];
-        }
-
-        else if (cache[keys[i]] === max) {
-            maxKey = Math.max(maxKey, keys[i]);
-        }
-    }
-
-    return (`The number that appears the most is: ${maxKey}`);
+    
 };
 
-console.log(mode([3, 2, 4, 3, 4, 4, 2, 2, 2, 3, 3, 3]));
-console.log(mode([7, 5, 8, 8, 2, 5,]));
 
 
 
@@ -72,13 +43,8 @@ mode([4, []]) -> 4
 */
 
 const modeNested = array => {
-    const newArray = array.flat(Infinity)
-    return mode(newArray);
+    
 };
-
-console.log(modeNested([[3], [2, [4]], 3]));
-console.log(modeNested([7, [[5, [8], 8], 2, 5]]));
-console.log(modeNested([4, []]));
 
 
 
