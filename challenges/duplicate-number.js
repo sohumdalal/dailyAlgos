d6481c09564064405cd560bcf0948a802f721f8c
sohumdalal <dsohum@gmail.com>
2023-12-01 11:53:15 +0530

    extension for duplicate done with constant time + psuedo

--- challenges/duplicate-number.js
+++ challenges/duplicate-number.js
@@ -4,29 +4,31 @@ One number is duplicated. Find it.
 ex: [1,5,4,3,6,2,4,7] should return 4
 */
 
+const { resourceLimits } = require("worker_threads");
+
 const duplicateNumber = array => {
-//my first inclination here is to use some of a cache that tracks how many times a number repeats
-//we know that only one num is duplicated, all we do if find the cache value that is not 1, and return the element
+    //my first inclination here is to use some of a cache that tracks how many times a number repeats
+    //we know that only one num is duplicated, all we do if find the cache value that is not 1, and return the element
 
-//edge case
-if(!array.length) {
-    return "please enter valid array length"
-}
-
-//define a cache and a dup variable
-const cache = {};
-let dup;
-
-//iterate and find the duplicate
-array.forEach(element => {
-    cache[element] = (cache[element] || 0) + 1;
-    if(cache[element] !== 1) {
-        dup = element;
+    //edge case
+    if (!array.length) {
+        return "please enter valid array length"
     }
-});
 
-//return it
-return dup;
+    //define a cache and a dup variable
+    const cache = {};
+    let dup;
+
+    //iterate and find the duplicate
+    array.forEach(element => {
+        cache[element] = (cache[element] || 0) + 1;
+        if (cache[element] !== 1) {
+            dup = element;
+        }
+    });
+
+    //return it
+    return dup;
 };
 
 
@@ -37,9 +39,32 @@ ex: [3, 4, 7, 6, 8, 5, 6] should return 6
 */
 
 const duplicateNumberAdvanced = array => {
-// return duplicateNumber(array);
-//upper = math.min + (array.length - 1) - 1; 
-//range is math.min to upper; 3 to 8;
+
+    //call this a duplicate variable
+    let duplicate;
+
+    //enter an interation over the array
+    for (let i = 0; i < array.length; i++) {
+        //lets call an index variable
+        let index = Math.abs(array[i]) - 1;
+        // console.log("i:", i)
+        // console.log("array[i]:", array[i])
+        // console.log("index:", index)
+        // console.log("array[index]:", array[index])
+        // console.log("--------------------")
+        //check if the array at such index is negative, if it is, then its a duplicate, reassign and then break
+        if (array[index] < 0) {
+            duplicate = Math.abs(array[i]);
+            break;
+        }
+        //if not neg, then make it negative, so if we come across that value again in our iteration, its a duplicate
+        else {
+            array[index] = -array[index];
+        }
+    }
+    return duplicate;
 };
+
+duplicateNumberAdvanced([3, 4, 7, 6, 8, 5, 6]);
 
 module.exports = { duplicateNumber, duplicateNumberAdvanced };
