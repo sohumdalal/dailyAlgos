bd0a3c2f7278cc997c8b4e3e813861153b7ab363
sohumdalal <dsohum@gmail.com>
2023-12-06 21:01:45 -0800

    psuedocode add longest common prefix

--- challenges/longest-common-prefix.js
+++ challenges/longest-common-prefix.js
@@ -9,16 +9,24 @@ longestCommonPrefix(["dog","racecar","car"]) --> "" (There is no common prefix a
 */
 
 const longestCommonPrefix = (strs) => {
+    //begin with an empty string, prefix
     let prefix = '';
+    //if empty string, then return the empty string, or prefix
     if (!strs.length) return prefix;
+    //so we want to first iterate over the first string in the array
     for (let i = 0; i < strs[0].length; i++) {
+        //lets check each character in the first string aginst the other strings in the array, and their first characters
         for (let j = 0; j < strs.length; j++) {
+            //if commonality is broken, then return the prefix
             if (strs[0][i] !== strs[j][i]) {
                 return prefix;
            }
         }
+        //if every ith position char, for each string, is the same. then add to prefix
         prefix += strs[0][i];
     }
+
+    //if the array is full of identical strings, then this return statement will trigger
     return prefix;
 }
 
