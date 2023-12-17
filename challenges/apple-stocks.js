fc18773cb84497b244371bc2e1e53f5c16fd2a28
sohumdalal <dsohum@gmail.com>
2023-12-16 21:40:35 -0800

    comments added

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -21,29 +21,30 @@ calculate profit along the array.
 */
 
 const highestProfit = apple_stock => {
-
+    //if the the input is not an array, or if there is less than two elements, then return 0;
     if (!Array.isArray(apple_stock) || apple_stock.length < 2) {
         return 0;
     }
 
+    //asign a min pointer, and a max value to track the maximum profit
     let minIndex = 0;
     let max = 0;
+
+    //iterate through the array, in order to find the maximum profit
     for (let i = 0; i < apple_stock.length; i++) {
-        // for (let j = i + 1; j < apple_stock.length; j++) {
-        //     const currentProfit = apple_stock[j] - apple_stock[i]
-        //     if (max < currentProfit) {
-        //         max = currentProfit
-        //     }
-        // }
+        //at every iteration check the profit, depending on if profit is negative you have one of two conditionals, and then you keep moving through the array
         const currentProfit = apple_stock[i] - apple_stock[minIndex];
         if(apple_stock[i] < apple_stock[minIndex]) {
+            //this is crucial, you keep reassigning the minIndex whereever the stock is lower than the previous apple_stock[minIndex]; yuo are keeping track of the min itself
             minIndex = i;
         }
         else if (currentProfit > max) {
+            //all your doing is reassigning the max, making sure you are checking at each iteration
             max = currentProfit;
         }
 
     }
+    //finally the max is recieved
     return max;
 }
 
