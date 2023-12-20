d6cd08b0dfded70048de5a230c6633433ff0512d
sohumdalal <dsohum@gmail.com>
2023-12-20 15:19:11 -0800

    perm palin with comments

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -15,29 +15,38 @@
  */
 
 const permPalin = str => {
+    //first edge out the case in which anything but a string is inputted by the user
     if(typeof str !== "string") {
         return false;
     }
-    //stick into an object and the pattern we are looking for is:
-    //if only one property in the object is 1, and all the other are the same number, you have a palindrome
-    // {a:2, b: 2} OR {a:2, b: 2, c:2, d:1}
 
+    //Now in order to do any caching logic, we should split the string, create a cache, and populate
+    //we ve done this alot, but look at sky's method, so gorgeous 
     const stringArray = str.split('');
     const cache = {}
     stringArray.forEach(element => {
         cache[element] = (cache[element] || 0) + 1;
     });
 
+    //here is where it gets fun, and took me some time to iron out:
+    //in order for you to have a palindrome, you need symetrry, which means that every letter is
+    //repeated an even number of times, or you have one character repeated once, while everyone else
+    //is repeated an even number of times. so racecar, teammate, sheesh can all be palindrome, based off
+    //their permutations
+
+    //to start initialize a var oneCount to be 0. if you come across a 1 in the cache, increment it.
     let oneCount = 0;
     for (let key in cache) {
         if (cache[key] === 1) {
             oneCount++
            }
-        else if(cache[key] % 2 !== 0) {
+        //this is the money, this ensures symmetrry in our palindrome, if you dont mod 2, then return false ASAP
+        else if (cache[key] % 2 !== 0) {
             return false;
         }
     }
 
+    //only return true if either of these two conditions are met, because only then is symmetry preserved
     return oneCount === 1 || oneCount === 0;
 
 };
@@ -45,7 +54,7 @@ const permPalin = str => {
 console.log(permPalin("abab"));
 console.log(permPalin("cbaba"));
 console.log(permPalin("cbac"));
-console.log(permPalin("a"));
+console.log(permPalin("abc"));
 
 
 /* 
