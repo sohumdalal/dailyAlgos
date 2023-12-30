3c3bc730450b1f2d0de136cb8da40f5d26e2017e
sohumdalal <dsohum@gmail.com>
2023-12-29 22:01:04 -0500

    kth to last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -1,19 +1,19 @@
 /**
- * Write a function that takes two parameters (an integer, and the head of a
+ * Writa function that takes two parameters (an integer, and the head oa
  * singly linked list) and returns the VALUE on the kth to last node in the list.
  *
- * const a = new Node('A');
- * const b = new Node('B');
- * const c = new Node('C');
- * const d = new Node('D');
- * const e = new Node('E');
+ consa = new Node('A');
+ const b = new Node('B');
+ const c = new Node('C');
+ const d = new Node('D');
+ const e = new Node('E');
  *
- * a.next = b;
+ a.next = b;
  * b.next = c;
  * c.next = d;
  * d.next = e;
  *
- * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
+ * kthToLastNode(2a); -> returns 'D' (the value on the second to last node)
  * 
  * Additional Information:
  *  - invalid inputs should return undefined
@@ -25,9 +25,46 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head){
+function kthToLastNode(k, head) {
+  let totalCount = 0;
+  function getLength() {
+    let curr = head;
+    while (curr) {
+      totalCount++;
+      curr = curr.next;
+    }
+  }
+  getLength();
+
+  const trav = totalCount - k;
+  let travCount = 0;
+  let curr = head;
+
+  while (curr) {
+    if(travCount === trav) {
+      return curr.value;
+    }
+    travCount++;
+    curr = curr.next;
+  }
+
+
 
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a));
+
+
 //Do not delete! 
-module.exports = {Node, kthToLastNode};
+module.exports = { Node, kthToLastNode };
