d68d02d03d51a63bcc0137bb27bab0b689abb06d
sohumdalal <dsohum@gmail.com>
2023-12-29 22:46:08 -0500

    max-depth notes

--- challenges/max-depth.js
+++ challenges/max-depth.js
@@ -23,16 +23,22 @@ const { arrayBuffer } = require("stream/consumers");
 
 const maxDepth = (arr, count = 1) => {
  
+  //start by defining a more global var called max which will keep track of the max depth
   let max = count;
 
   for(const el of arr) {
+    //iterating through the array, lets check each element if it is an array
     if(Array.isArray(el)) {
+      //if so, declare a var depth, which returns out a local max of nested arrays, and assigns it to a depth var
       const depth = maxDepth(el, count + 1);
       if (depth > max) {
+        //reassign the global max to the depth var, if it truly is the max, else dont do anything with it, and move on in the loop.
         max = depth;
       }
     }
   }
+
+  //when we finally are finished with all our nested arrays, and the loop is finished. we return max, which since it is defined more globally, returns the true max depth.
 
   return max;
 
