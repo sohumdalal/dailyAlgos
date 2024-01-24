806aafb3f40c4799d8a0b24db494f61dfeabd84c
sohumdalal <dsohum@gmail.com>
2024-01-24 18:33:00 -0500

    finished bubble

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -1,12 +1,34 @@
 
 
-function bubbleSort (array) {
+function bubbleSort(array) {
 
+    //the first thing Im thinking about is swapping elements, as
+    //we progress down the array
 
+    //first declare a loop
+    for (let i = 0; i < array.length; i++) {
 
+        console.log("i:", i)
+        console.log("Array@i:", array[i]);
 
+        for (let j = 0; j < array.length - 1; j++) {
+
+            console.log("Array@j:", array[j]);
+            console.log("Array@j+1:", array[j + 1]);
+            console.log("----------------------")
+
+            if (array[j] > array[j + 1]) {
+                const lower = array[j + 1]
+                array[j + 1] = array[j];
+                array[j] = lower;
+            }
+            console.log("My Array is:", array)
+        }
+    }
+    return array;
 }
 
+console.log(bubbleSort([1, 4, 2, 5, -2, 3]))
 
 
 
